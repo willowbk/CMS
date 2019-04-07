8350a3ad9f6d76e0c04b724f03723aadf9edaffb
Willow Kion-Crosby <wbk7@physics.rutgers.edu>
2019-04-07 15:22:26 -0400

    changed example to line with parabola

--- Examples/ExFunc1.py
+++ Examples/ExFunc1.py
@@ -3,26 +3,18 @@ from CMS import optimize as opt
 
 def func1(x):
 	"""
-	func1 returns the distance between a
-	point on the plane z = m1*x + m2*y + b
-	and the origin of the coordinate system.
+	func1 returns the distance between
+	a parabola and a line.
 	"""
 	
-	m1 = 2
-	m2 = -1
-	b = 0
+	return abs((x[0] - 1)**2 + 1 - x[0])
 	
-	z = m1*x[0] + m2*x[1] + b
+init_xs = [[-1 + .1*i] for i in range(20 + 1)]
+
+for init_x in init_xs:
 	
-	return (x[0]**2 + x[1]**2 + z**2)**.5
-	return abs((x[0] - 1)**2 - x[0])
+
+	x,func_val = opt.CMS(func1, init_x)
+
+	print(init_x, x, func_vale)
 	
-#func, init_x, max_x=None, min_x=None, tail=25, tmax=10**3, depth=2, init_dx=None, eps=1e-8
-
-init_x = [-.5, 1.5]
-
-
-x,func_val = opt.CMS(func1, init_x)
-
-print(x)
-print(func_val)
